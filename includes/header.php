0d1dfb661d632ce28d3b657c0d03fba718889a79
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 11:34:27 +0545

    Changes made in All

--- includes/header.php
+++ includes/header.php
@@ -29,12 +29,12 @@
             <div class="Header_Image">
                 <img src="img/Slider_image_1.png" alt="Welcome to Naava Hotel" width="100%">   
             </div>
-            <div class="container mr-5 Header_Content">
+            <div class="container mr-5 Header_Logo_Content">
                 <div class="row">
-                    <div class=col>
+                    <div class=col-2>
                         <img src="img/Naava PP.png" alt="Naava Brown Bg logo Image">
                     </div>
-                    <div class="col-10">
+                    <div class="col-10 Header_Content">
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
