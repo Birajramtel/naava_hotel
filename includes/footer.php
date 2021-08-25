23dda57d8ec390f475ffefdd530ee9be776855c3
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 15:48:26 +0545

    Changes made in footer page

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,27 @@
+        <div class="container mt-5 ">
+            <div class="row mt-5">
+                <div class="col-6 mt-5">
+                    <h6>PHOTOS</h6>
+                    <h2> <strong>See our latest photos <br>From restaurant and hotel </strong></h2>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/Table_final.png" class="img-fluid" alt="Table Final">
+                </div>
+                <div class="col-3">
+                    <img src="img/Bishnudwaar_final.png" class="img-fluid float-right" alt="Bishnudwaar Hiking">
+                </div>
+                <div class="col-3">
+                    <img src="img/Bed_final.png" class="img-fluid float-left" alt="Hotel Bed">
+                </div>
+                <div class="col-3">
+                    <img src="img/Seminar_final.png" class="img-fluid float-right" alt="Seminar Hall"> 
+                </div>                    
+            </div>
+        </div>      
         <footer class="footer_hero">
         <div class="container mt-5 ">
                 <div class="row mt-5">
