723b098e2c5399b343d6f33d01b5d4a09a803c3e
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 17:34:57 +0545

    Changes made in eat and Drink Page

--- eat_and_drink.php
+++ eat_and_drink.php
@@ -1,9 +1,30 @@
 <?php
-    include("include/header.php")
+    include("includes/header.php")
 ?>
 
-
+    <div class="container mt-5 ">
+        <div class="row">
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-6">
+                        <img src="img/eat_and_drink_1.png" alt="Accommodation Rooms">
+                    </div>
+                    <div class="col-6">
+                        <img src="img/Accommodation room 2.png" alt="Accommodation Rooms">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2> <strong>Visit us to eat and <br>Drink Fresh food</strong></h2>
+        <p class="paragraph_align">
+            <strong>
+                At Naava, we serve you high quality food with variety and fresh local ingredients.
+                We serve multi-cuisine food menu with authentic Thakali meal set, Nepali food, 
+                Indian dishes, popular Chinese and Indian dishes with local and imported beverages.
+            </strong>
+        </p>
+    </div>
 
 <?php
-    include("include/footer.php")
+    include("includes/footer.php")
 ?>
