3785b9565281454417a676cac64401b47cd1859a
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 10:55:07 +0545

    Changes made in Footer Page

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,41 @@
         <footer class="footer_hero">
-            <p> Hello World</p>
+        <div class="container mt-5 ">
+                <div class="row mt-5">
+                    <div class="col-3 mt-5 pr-3">
+                        <h2> <strong>Naava Resort and Retreat </strong></h2>
+                        <p><strong>Our hotel is an image of quality and profesionalism combined with hard work and
+                            percision. You can except only the best services form our staff.
+                        </strong></p>
+                    </div>
+                    <div class="col-3 mt-5 pb-2">
+                        <h2> <strong>Useful Links</strong></h2>
+                        <ul>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Rooms</a></li>
+                            <li><a href="#">Gift Card</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3 mt-5 pb-2">
+                        <h2> <strong>Privacy</strong></h2>
+                        <ul>
+                            <li><a href="#">Career</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Services</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3 mt-5 pb-2">
+                        <h2> <strong>Contact Info</strong></h2>
+                        <ul>
+                            <li>example@naava.com</li>
+                            <li>+0 320 422 4254</li>
+                            <li>Main Str Nepal</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
         </footer>
     </body>
 </html>
