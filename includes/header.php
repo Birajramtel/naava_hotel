2367c651fedcfa7105253e638b03a45d800fd805
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 11:19:59 +0545

    Changes made in Header Page

--- includes/header.php
+++ includes/header.php
@@ -30,45 +30,49 @@
                 <img src="img/Slider_image_1.png" alt="Welcome to Naava Hotel" width="100%">   
             </div>
             <div class="container mr-5 Header_Content">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <div>
-                            <img src="img/Naava PP.png" alt="Naava Brown Bg logo Image">
-                        </div>
-                        <ul class="navbar-nav">
-                            <li class="nav-item pl-2">
-                                <a class="nav-link active" href="index.html"><u> Home</u><span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About Naava</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Explore Shivapuri</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Accommodation</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Eat and Drink</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Packages</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Highlights</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Gallery</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
-                            </li>
-                        </ul>
+                <div class="row">
+                    <div class=col>
+                        <img src="img/Naava PP.png" alt="Naava Brown Bg logo Image">
                     </div>
-                </nav>
+                    <div class="col-10">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav pl-2">
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link active" href="index.html"><u> Home</u><span class="sr-only">(current)</span></a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">About Naava</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Explore Shivapuri</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Accommodation</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Eat and Drink</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Packages</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Highlights</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Gallery</a>
+                                    </li>
+                                    <li class="nav-item pl-2">
+                                        <a class="nav-link" href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
