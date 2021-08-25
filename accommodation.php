748ae7ece0e3a6391cd34f9c8a0159e376abcef2
Birajramtel <biraj.ramtel007@gmail.com>
2021-08-25 15:46:52 +0545

    Accommodation page added

--- accommodation.php
+++ accommodation.php
@@ -1,7 +1,38 @@
 <?php
 include('includes/header.php')
 ?>
-
+<div class="container mt-5 ">
+    <h2> <strong>Best Place <br> Best View.</strong></h2>
+        <p class="paragraph_align"><strong>
+            Rooms surrounded by the natural vegetation of Shivapuri National Park in one elevation and overlooking
+            Kathmandu valley on the other, Naava is a place where you can have quiet, peaceful and private resting. 
+            Our Standard Rooms have a large bed, private bathroom, shower products, LED TV with satellite channels, 
+            Tea / Coffee makers, hot water and free high-speed wireless internet. The rooms that have veranda where 
+            you can relax and have pleasure that will you feel at home, away from home. <br>
+            The spectacular weather with a sip of fresh air and a breezy gulp of air inside make you pristine where 
+            you can relax in the garden and hear chirping of birds and other natural habitat in the national park.
+            Shivapuri is a naturally and ecologically rich destination for tourists seeking to escape the hustle and 
+            bustle of Kathmandu city and an ideal stop-off point for hiking to the national part. One can indulge in 
+            all that nature has to offer as you experience the captivating sunrise views in the tranquil ambience of
+            the resort. Spread over an expansive area of over 20,000sq.ft. of total space, living here can give you 
+            experience of home in the hills.</strong>
+        </p>
+            
+        <div class="row">
+            <div class="col-3">
+                <img src="img/Accommodation room 1.png" alt="Accommodation Rooms">
+            </div>
+            <div class="col-3">
+                <img src="img/Accommodation room 2.png" alt="Accommodation Rooms">
+            </div>
+            <div class="col-3">
+                <img src="img/Accommodation room 3.png" alt="Accommodation Rooms">
+            </div>
+            <div class="col-3 pr-0">
+                <img src="img/Accommodation room 4.png" alt="Accommodation Rooms">
+            </div>
+        </div>
+    </div>
 <?php
 include('includes/footer.php')
 ?>
